37ea86de2562d65bc04e394a0fb60d74b8fb7696
zhu <1103048750@qq.com>
2019-05-21 21:58:04 +0800

    3.12新闻列表展现和添加头布局

--- zhibj149/app/src/main/java/com/qq149/zhibj149/base/impl/menu/TabDetailPager.java
+++ zhibj149/app/src/main/java/com/qq149/zhibj149/base/impl/menu/TabDetailPager.java
@@ -7,7 +7,9 @@ import android.support.v4.view.ViewPager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -54,9 +56,15 @@ public class TabDetailPager extends BaseMenuDetailPager {
     @ViewInject(R.id.tv_title)
     private TextView tvTitle;
 
+    @ViewInject(R.id.lv_list)
+    private ListView lvList;
+
+
     private String mUrl;
 
     private ArrayList<NewsTabBean.TopNews> mTopNews;
+    private ArrayList<NewsTabBean.NewsData> mNewsList;
+    private NewsAdapter mNewsAdapter;
 
 
     public TabDetailPager(Activity activity, NewsTabData newsTabData) {
@@ -77,6 +85,12 @@ public class TabDetailPager extends BaseMenuDetailPager {
         view.setGravity(Gravity.CENTER);*/
         View view = View.inflate(mActivity, R.layout.pager_tab_detail, null);
         ViewUtils.inject(this, view);
+
+        //给listview添加头布局
+        View mHeaderView = View.inflate(mActivity,R.layout.list_item_header,null);
+        ViewUtils.inject(this, mHeaderView);//此处将头布局也注入
+        lvList.addHeaderView(mHeaderView);
+
         return view;
     }
 
@@ -153,6 +167,14 @@ public class TabDetailPager extends BaseMenuDetailPager {
             tvTitle.setText(mTopNews.get(0).title);
             //默认让第一个选中（解决页面销毁后重新初始化时，Indicator仍然保留上次圆点位置的bug）
             mIndicator.onPageSelected(0);
+
+            //列表新闻设置
+            mNewsList =newsTabBean.data.news;
+            if (mNewsList!=null){
+                mNewsAdapter = new NewsAdapter();
+                lvList.setAdapter(mNewsAdapter);
+            }
+
         }
     }
 
@@ -179,7 +201,7 @@ public class TabDetailPager extends BaseMenuDetailPager {
         public Object instantiateItem(@NonNull ViewGroup container, int position) {
             ImageView view = new ImageView(mActivity);
             //设置默认图片
-           // view.setImageResource(R.drawable.topnews_item_default);//没用
+            // view.setImageResource(R.drawable.topnews_item_default);//没用
             view.setScaleType(ImageView.ScaleType.FIT_XY);//设置图片缩放方式，宽高填充父控件
             String imageUrl = mTopNews.get(position).topimage;//图片下载链接
 
@@ -195,6 +217,56 @@ public class TabDetailPager extends BaseMenuDetailPager {
         public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
             container.removeView((View) object);
         }
+    }
+
+    class NewsAdapter extends BaseAdapter{
+
+        public NewsAdapter(){
+            mBitmapUtils = new BitmapUtils(mActivity);
+            mBitmapUtils.configDefaultLoadingImage(R.drawable.news_pic_default);
+        }
+
+        @Override
+        public int getCount() {
+            return mNewsList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mNewsList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHoder holder;
+            if (convertView == null){
+                convertView = View.inflate(mActivity,R.layout.list_item_news,null);
+                holder = new ViewHoder();
+                holder.ivIcon = convertView.findViewById(R.id.iv_icon);
+                holder.tvTitle = convertView.findViewById(R.id.tv_title);
+                holder.tvDate = convertView.findViewById(R.id.tv_date);
+
+                convertView.setTag(holder);
+            }else {
+                holder = (ViewHoder) convertView.getTag();
+            }
+            NewsTabBean.NewsData news = (NewsTabBean.NewsData) getItem(position);
+            holder.tvTitle.setText(news.title);
+            holder.tvDate.setText(news.pubdate);
+
+            mBitmapUtils.display(holder.ivIcon,news.listimage);
+            return convertView;
+        }
+    }
+    static class ViewHoder{
+        public ImageView ivIcon;
+        public TextView tvTitle;
+        public TextView tvDate;
     }
 
 }
